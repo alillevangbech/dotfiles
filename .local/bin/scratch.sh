1714f77e9af3545be6cbfe80c51961e26163f1d1
Alexander <alexanderlillevangbech@gmail.com>
2021-02-20 19:16:30 +0100

    new style

---
+++ .local/bin/scratch.sh
@@ -0,0 +1,54 @@
+## Places: ARCH PACKAGE "" | AUR "A" | GITHUB "G" | OTHER "O"
+## expects pacstrap /mnt base base-devel linux linux-firmware nvim
+## csharp - mono && monobuild + install scripts. 
+
+zsh | 
+tmux |
+neovim |
+cmake |
+pass |
+pulsemixer |
+pavucontrol |
+syncthing |
+networkmanager |
+fzf |
+neomutt |
+newsboat |
+man-db |
+noto-fonts-emoji |
+ttf-linux-libertine |
+pulseaudio-alsa |
+unzip |
+
+zathura |
+zathura-pdf-mupdf |
+poppler |
+gnome-keyring |
+
+xcape |
+xclip |
+xdotool |
+xorg-server |
+xorg-xinit |
+
+## temp /*
+networkmanager-openconnect |
+network-manager-applet |
+openconnect |
+trayer |
+## */
+
+## AUR
+yay
+brave-bin
+spotify
+gtk-theme-arc-gruvbox-git
+
+## GIT
+st
+dwm
+dmenu
+slbar
+
+
+
