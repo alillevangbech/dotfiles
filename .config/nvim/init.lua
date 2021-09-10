a32ebb5410310139a34f7c7ac60a10ff28fc3cf8
Alexander Bech <alexanderlillevangbech@gmail.com>
2021-09-10 16:04:03 +0200

    Dos endings and all that crap

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,6 +4,8 @@ local fn = vim.fn    -- to call Vim functions e.g. fn.bufnr()
 local g = vim.g      -- a table to access global variables
 local opt = vim.opt  -- to set options
 
+g.mapleader = ','
+
 local function map(mode, lhs, rhs, opts)
   local options = {noremap = true}
   if opts then options = vim.tbl_extend('force', options, opts) end
@@ -11,21 +13,33 @@ local function map(mode, lhs, rhs, opts)
 end
 
 -------------------- PLUGINS -------------------------------
--- paq install
--- git clone --depth=1 https://github.com/savq/paq-nvim.git $HOME/.local/share/nvim/site/pack/paqs/start/paq-nvim 
-cmd 'packadd paq-nvim'
-local paq = require('paq-nvim').paq
-paq {'neovim/nvim-lspconfig'}
-paq {'hrsh7th/nvim-compe'}
-paq {'nvim-treesitter/nvim-treesitter'}
-paq {'vim-airline/vim-airline'}
-paq {'vim-airline/vim-airline-themes'}
-paq {'morhetz/gruvbox'}
-paq {'savq/paq-nvim', opt = true}
+local Plug = fn['plug#']
+
+vim.call('plug#begin', '~/.config/nvim/plugged')
+
+Plug 'vim-airline/vim-airline'
+Plug 'vim-airline/vim-airline-themes'
+Plug 'morhetz/gruvbox'
+
+Plug('junegunn/fzf', {['do'] = fn['fzf#install']})
+Plug 'junegunn/fzf.vim'
+
+Plug 'neovim/nvim-lspconfig'
+Plug 'hrsh7th/nvim-cmp'
+Plug 'hrsh7th/cmp-nvim-lsp'
+Plug 'hrsh7th/cmp-buffer'
+Plug 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
+Plug 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
+Plug 'L3MON4D3/LuaSnip' -- Snippets plugin
+Plug 'ray-x/lsp_signature.nvim'
+Plug 'ojroques/nvim-lspfuzzy'
+
+
+vim.call('plug#end')
 
 -------------------- OPTIONS -------------------------------
 cmd 'colorscheme gruvbox'
-opt.completeopt = {'menuone', 'noinsert', 'noselect'}
+opt.completeopt = {'menuone', 'noselect'}
 opt.wildmode = {'list', 'longest'}
 opt.pumheight = 15 
 opt.number = true
@@ -39,26 +53,100 @@ opt.ignorecase = true
 opt.termguicolors = true            -- True color support
 
 -------------------- MAPPINGS ------------------------------
-map('i', '<S-Tab>', 'pumvisible() ? "\\<C-p>" : "\\<Tab>"', {expr = true})
-map('i', '<Tab>', 'pumvisible() ? "\\<C-n>" : "\\<Tab>"', {expr = true})
+-- Make
+map('n', '<leader>b',"<cmd>w | !make<CR>")
+map('s', '<tab>',"c")
+map('n', '<tab>',"f*xi")
+
+-- keyboard slow
+
+cmd 'command! Q quit'
+cmd 'command! W write'
+cmd 'command! Wq wq'
 
 -------------------- PLUGIN SETUP ---------------------------
-require('compe').setup {
-  min_length = 2,
-  preselect = 'disable',
-  source = {buffer = true, path = true, nvim_lsp = true, omni = {filetypes = {'tex'}}},
+require 'lspfuzzy'.setup {}
+
+local luasnip = require 'luasnip'
+local cmp = require 'cmp'
+cmp.setup {
+	completion = {
+		completeopt = 'menu,menuone,noinsert',
+		keyword_length = 2,
+	},
+	snippet = {
+		expand = function(args)
+			require('luasnip').lsp_expand(args.body)
+		end,
+	},
+	mapping = {
+		['<Up>'] = cmp.mapping.select_prev_item(),
+		['<Down>'] = cmp.mapping.select_next_item(),
+		['<Tab>'] = cmp.mapping.confirm {
+			behavior = cmp.ConfirmBehavior.Replace,
+			select = true,
+		},
+	},
+	sources = {
+		{ name = 'nvim_lsp' },
+		{ name = 'buffer' },
+		{ name = 'luasnip' },
+	},
 }
 
 -------------------- LSP -----------------------------------
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.documentationFormat = { 'markdown', 'plaintext' }
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+capabilities.textDocument.completion.completionItem.preselectSupport = true
+capabilities.textDocument.completion.completionItem.insertReplaceSupport = true
+capabilities.textDocument.completion.completionItem.labelDetailsSupport = true
+capabilities.textDocument.completion.completionItem.deprecatedSupport = true
+capabilities.textDocument.completion.completionItem.commitCharactersSupport = true
+capabilities.textDocument.completion.completionItem.tagSupport = { valueSet = { 1 } }
+capabilities.textDocument.completion.completionItem.resolveSupport = {
+	properties = {
+		'documentation',
+		'detail',
+		'additionalTextEdits',
+	},
+}
+
 for ls, cfg in pairs({
-	pylsp = {}, clangd = {},
+	pylsp = {},
+	clangd = { 
+		capabilities = capabilities,
+		on_attach = on_attach,
+ 		handlers = {
+ 			["textDocument/publishDiagnostics"] = vim.lsp.with(
+ 			vim.lsp.diagnostic.on_publish_diagnostics, {
+ 				virtual_text = false
+ 			}),
+ 		},
+	},
 }) do require('lspconfig')[ls].setup(cfg) end
+
+require'lsp_signature'.setup {
+	bind = true,
+	doc_lines = 5,
+	floating_window = true,
+	hint_enable = false,
+	handler_opts = {border = "single"},
+	extra_trigger_chars = {"(", ","},
+}
+
+-- Movement
 map('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>')
 map('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>')
 map('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>')
 map('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>')
+map('n', 'ge', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>')
+
+-- Info
+map('n', '<C-q>', '<cmd>lua vim.lsp.buf.workspace_symbol()<CR>')
+map('n', '<C-a>', '<cmd>lua vim.lsp.buf.code_action()<CR>')
+map('n', '<C-f>', '<cmd>lua vim.lsp.buf.formatting()<CR>')
+map('n', '<C-h>', '<cmd>lua vim.lsp.buf.hover()<CR>')
    
-
-
 
 
--- .config/zsh/.zshrc
+++ .config/zsh/.zshrc
@@ -9,14 +9,39 @@ zmodload zsh/complist
 compinit
 _comp_options+=(globdots)		# Include hidden files.
 
-# SSH & GPG
-unset SSH_AGENT_PID
-if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
-  export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
-fi
+# # SSH & GPG
+# unset SSH_AGENT_PID
+# if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
+#   export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
+# fi
 
-export GPG_TTY=$(tty)
-gpg-connect-agent updatestartuptty /bye >/dev/null
+# export GPG_TTY=$(tty)
+# gpg-connect-agent updatestartuptty /bye >/dev/null
+
+# bindkey -v
+# export KEYTIMEOUT=1
+# 
+# bindkey -M menuselect 'h' vi-backward-char
+# bindkey -M menuselect 'k' vi-up-line-or-history
+# bindkey -M menuselect 'l' vi-forward-char
+# bindkey -M menuselect 'j' vi-down-line-or-history
+# bindkey -v '^?' backward-delete-char
+# 
+# function zle-keymap-select () {
+# 	case $KEYMAP in
+# 		vicmd) echo -ne '\e[1 q';;      # block
+# 		viins|main) echo -ne '\e[5 q';; # beam
+# 	esac
+# }
+# 
+# zle -N zle-keymap-select
+# zle-line-init() {
+#     zle -K viins # initiate `vi insert` as keymap (can be removed if `bindkey -V` has been set elsewhere)
+#         echo -ne "\e[5 q"
+# }
+# zle -N zle-line-init
+# echo -ne '\e[5 q' # Use beam shape cursor on startup.
+# preexec() { echo -ne '\e[5 q' ;} # Use beam shape cursor for each new prompt.
 
 HISTSIZE=10000000
 SAVEHIST=10000000
@@ -24,3 +49,5 @@ HISTFILE=~/.cache/zsh/history
 
 source "$XDG_CONFIG_HOME"/shell/aliasrc
 source "$XDG_CONFIG_HOME"/shell/profile
+source "$XDG_CONFIG_HOME"/shell/saxo_profile
+source /usr/share/zsh/plugins/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh 2>/dev/null
